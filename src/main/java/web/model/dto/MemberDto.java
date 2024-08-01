0613ea966e539b50fe8a9caffa85f4c9b3c520cf
limmmmmmmmmmmmmmmmm <lhk5324@naver.com>
2024-08-01 18:00:43 +0900

    헤더

--- src/main/java/web/model/dto/MemberDto.java
+++ src/main/java/web/model/dto/MemberDto.java
@@ -1,15 +1,19 @@
 package web.model.dto;
 
 
+import lombok.*;
 
+@Builder@Getter@Setter
+@ToString@NoArgsConstructor
+@AllArgsConstructor
 public class MemberDto {
-
-    private String mid;
-    private String mpw;
-    private String mname;
-    private String mphone;
-    private String mgender;
-    private String mbirth;
-    private String maccount;
+    private String mno;   //회원번호
+    private String mid;   //회원ID
+    private String mpw;   //회원PW
+    private String mname; //회원 이름
+    private String mphone;   //회원 연락처
+    private String mgender;  //회원 성별
+    private String mbirth;   //회원 생년원일
+    private String maccount; //회원 계좌번호
 
 }
