33803840cab7cf6d3f3cc4b2ea536fb596d3c0c8
limmmmmmmmmmmmmmmmm <lhk5324@naver.com>
2024-08-02 17:08:40 +0900

    헤더ㄴㅇ

--- src/main/java/web/controller/ViewController.java
+++ src/main/java/web/controller/ViewController.java
@@ -6,15 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
 
-    // [1] 회원가입
-    @GetMapping("/member/signup")
-    public String OffSidSignup(){ return "/member/signup.html"; }
-    // ======== [1] 레이아웃 =============== //
     @GetMapping("/")    // http://localhost:8080  // 페이지 요청은 HTTP의 GET 방식을 주로 사용된다.
     public String index(){
         return "/index.html";   // templates 폴더내 반환할 경로와 파일명
     }
 
+    // ======== [2] 회원관련 =============== //
+    // [1] 회원가입
+    @GetMapping("/member/signup")
+    public String OffSidSignup(){ return "/member/signup.html"; }
 
 
     @GetMapping("/member/my/info")
