c95682ea9f719081932590e20ae459610b9d041e
tjwlsvy <tjwlsvy@gmail.com>
2024-08-07 16:19:34 +0900

    24-08-02 board 추가

---
+++ src/main/java/web/service/BoardService.java
@@ -0,0 +1,14 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.model.dao.BoardDao;
+
+@Service
+public class BoardService {
+    @Autowired
+    BoardDao boardDao;
+
+
+
+}
