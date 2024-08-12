291e0c19342318417349da49be16a7b363b1a100
tjwlsvy <tjwlsvy@gmail.com>
2024-08-12 17:14:20 +0900

    24-08-12 회원가입 수정

--- src/main/java/web/controller/ViewController.java
+++ src/main/java/web/controller/ViewController.java
@@ -55,8 +55,8 @@ public class ViewController {
     @GetMapping("/board/braed")
     public String bRead(){return "/board/update.html";}
 
-    //[4] 게시판 삭제 페이delet지 요청
-    @GetMapping("/board/e")
+    //[4] 게시판 삭제 페이지 요청
+    @GetMapping("/board/delete")
     public String bDelete(){
         return "/board/delete.html";
     }
