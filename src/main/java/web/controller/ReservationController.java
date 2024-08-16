66b7896b5a8c1ef5a1112bab8eaf8f4ba8b90e67
tjwlsvy <tjwlsvy@gmail.com>
2024-08-16 15:59:57 +0900

    24-08-16 구장 예약 취소

--- src/main/java/web/controller/ReservationController.java
+++ src/main/java/web/controller/ReservationController.java
@@ -1,9 +1,7 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import web.service.ReservationService;
 
 import java.util.List;
@@ -14,6 +12,25 @@ import java.util.Map;
 public class ReservationController {
     @Autowired ReservationService reservationService;
 
+    // 구장 예약
+    @PostMapping("/stadiumreservation") //   구장예약 신청 URL : /reservation/stadiumreservation?bno=구장번호
+    public boolean stadiumReservation(int bno){
 
+        return reservationService.stadiumReservation(bno);
+    } // end
+
+    // 구장 취소
+    @PutMapping("/stadiumcancellation")
+    public boolean StadiumCancellation(int bno){
+
+        return reservationService.StadiumCancellation(bno);
+    }// StadiumCancellation end
+
+    // 구장 예약 유효성 검사
+    @GetMapping("/effectiveness")
+    public boolean effectiveness(int bno ){
+        return reservationService.effectiveness(bno);
+
+    }// effectiveness end
 
 }
